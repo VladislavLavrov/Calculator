dc99c5eb3a8ae82a18882b8c6918553695a199f9
Владислав Лавров <55340643+VladislavLavrov@users.noreply.github.com>
2023-12-08 12:54:36 +0500

    1.23.12.01
    Обновил название кнопки

--- Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc65727689c0f8bbafa8684a2e9c42b2ea76848")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
