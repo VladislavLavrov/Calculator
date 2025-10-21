8cdb78513e4009a7ce3f2d723e56488343cea369
VladislavLavrov <lavrov.vladislav@gmail.com>
2025-10-21 12:17:49 +0500

    1.25.10.02
    Новая версия калькулятора

--- Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d3d5f236043b97f1c0046039f664424690fdf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4997a509ea13305ccbb96faab996109853eb733")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
