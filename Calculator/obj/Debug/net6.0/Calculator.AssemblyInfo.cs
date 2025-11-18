b21ff726a95406306febe46545d5ea9defc312c2
VladislavLavrov <lavrov.vladislav@gmail.com>
2025-11-18 11:05:04 +0500

    1.25.11.03
    Поменяли название калькулятора

--- Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d965b44ae8863127e03c6582b4b28891e33f7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa38276346d41555fa0aa376156c26733e0f5a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
